70cff22cc42f9183b65074837664083311121598
thienth32 <thienth32@gmail.com>
2017-10-13 08:05:04 +0700

    update using ajax

---
+++ l13/get-data.php
@@ -0,0 +1,14 @@
+<?php 
+$arr = [
+	['id' => 1, 'name' => 'chungnq', 'score' => 1],
+	['id' => 2, 'name' => 'tiennm', 'score' => 1.5],
+	['id' => 3, 'name' => 'lochl', 'score' => 0.5],
+	['id' => 4, 'name' => 'hieudk', 'score' => 2],
+	['id' => 5, 'name' => 'thienth', 'score' => 10],
+];
+
+sleep(10);
+echo json_encode($arr);
+
+
+ ?>
